c579f6dfcd5601ef8c2f85eeff80db0a1fbd5e27
Oksana Karpyn <oksana101992@gmail.com>
2023-09-07 14:23:39 +0200

    agg javascript

---
+++ assets/js/script.js
@@ -0,0 +1,143 @@
+// racogliamo tutti i elementi di interesse
+
+let totalPrice = document.querySelector('.total-price');
+let giftListElement = document.querySelector('.gifts-list');
+let form = document.getElementById('form-send');
+// campi del form
+let inputName = document.querySelector('#input-name') ;
+let inputPrice = document.querySelector('#input-price') ;
+let inputDescription = document.querySelector('#input-description') ;
+
+// prepariamo la lista scatola vuota dove inser dentro elem
+const gifts = [];
+ 
+//----------- eventi dinanici---------
+//attiviamo il form 
+form.addEventListener('submit',function(event){
+	//1.submit scat evento con enter o clik bottone
+	//2.funzione con istruzioni da eseguire
+	
+	//step1.blochiamo ricaricamento dell paggina perche vogliamo gestire con javascript
+	event.preventDefault();
+	console.log('invio');
+
+   //step 2. racogliere i dati del form
+	const name = inputName.value.trim();
+	const price = inputPrice.value.trim();
+	const description = inputDescription.value.trim();
+
+
+	//step 3. aggiungere regalo in lista
+	//creo funzione fuorui e lo riutilizzo
+	addGift(name,price,description);
+	//step 4. ripulire il form
+	form.reset();
+	//step 5 riportiamo il focus sul primo campo
+	inputName.focus();
+
+})
+// ---------functioni-----
+
+
+//funzione x agg un regalo all lista
+ function addGift(name,price,description){
+	//step 1. creare un oggetto che rapres un regalo 
+	const newGift = {
+		// propieta: valore
+		name :name,
+		price:Number(price),
+		description: description
+	}
+	console.log(newGift);
+	//step 2. agg l'oggetoo alla lista
+	gifts.push(newGift);
+	console.log(gifts);
+	//step 3. calcoliamo il totale
+	calcolateTotale();
+	//step 4. mostriamo in pag lista dei regali
+	mostraGift();
+ }
+
+
+ //funzione che calcola il totale 
+ function calcolateTotale(){
+	//mi preparo a calcolare 
+	let total = 0;
+	//per ogni regalo......
+	for(let i = 0; i < gifts.length; i++){
+		//total = total + gifts[i].price; 
+		//pi corto
+		total += gifts[i].price;
+	}
+	console.log(total);
+	//stampiamo in pag il totale
+	totalPrice.innerHTML = `${formatCifra(total)} €`; 
+
+ }
+
+ //funzione per formattare una cifra 
+ function formatCifra(cifra){
+	return cifra.toFixed(2);
+ }
+
+ //funzione per mostrare i regali in paggina
+  function mostraGift(){
+	// !todo ?? prima svottiamo  e pio recostruita
+	giftListElement.innerHTML = '';
+	//pre tutti regali...
+	for(let i = 0; i< gifts.length; i++){
+		//step 1. creo il codice per un singolo elemento della lista
+		const giftElement = createListElement(i);
+		// step 2. lo aggancio all lista della paggina
+		giftListElement.innerHTML +=  giftElement;
+		//step 3. rendo bottoni cliccabili
+		setDeleteBtn();
+		
+	}
+
+  };
+
+  //funzione per creare un elem della lista
+  function createListElement(i){
+	//restituishe il codice in html  di un gift
+	const gift = gifts[i];//quale regalo sto cercando di creare
+	 return `
+	<div class="gift">
+      <div class="gift-info">
+         <h3>${gift.name}</h3>
+         <p>${gift.description}</p>
+      </div>
+      <div class="gift_price">${formatCifra(gift.price)}€</div>
+      <button class="gift_btn" data-index="${i}">❌</button>
+   </div>
+	`
+  }
+  //adesso dobiamo eliminare elementi
+//   funzione che attiva i bottoni di eliminazione
+function setDeleteBtn(){
+//step 1. recupera tutti bottoni dei regali 
+const deleteButons = document.querySelectorAll('.gift_btn');
+//step 2. per ogni uno dei bottoni....
+	for(let i = 0; i < deleteButons.length; i++ ){
+	//step 3. recupero singolo bottone	
+	const button = deleteButons[i];
+
+		//step 4 agg adevebìntlistener
+		button.addEventListener('click',function(){
+			//step 5. individuo index del regalo
+			const indexBtn = button.dataset.index;
+			console.log(indexBtn);
+			//step 6. rimuovo dalla lista gift corrispondente
+			removeGift(indexBtn);
+		})
+	}
+}
+
+//funzione elimina gift
+function removeGift(index){
+	//rimuovo elemento dalla lista
+	gifts.splice(index,1)
+	console.log(gifts);
+	mostraGift();
+	calcolateTotale();
+}
